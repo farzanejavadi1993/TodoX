7a2c3a045870b480a6fa60779c22400a9bd37bc1
AriaRayane <farzaneh.javadi1992@gmail.com>
2025-07-24 14:12:55 +0200

    chore: connect Android app to Firebase and add google-services plugin

--- feature/tasklist/build.gradle.kts
+++ feature/tasklist/build.gradle.kts
@@ -38,9 +38,13 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
 
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling)
+    implementation(libs.androidx.material3)
     implementation(libs.lifecycle.viewmodel)
     implementation(libs.androidx.foundation.layout)
-    implementation(libs.androidx.material3.android)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
